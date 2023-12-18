351358583176674ff91b54ed6bc8fcb194eba696
Kisa <55666909+Kisa04@users.noreply.github.com>
2023-12-18 00:22:06 +0000

    add register logic

--- app2.js
+++ app2.js
@@ -431,10 +431,4 @@ helpme = Math.max(helpme, 0), window.addEventListener("DOMContentLoaded", (funct
                 }))
         }
     }))
-})); // This is just a sample script. Paste your real code (javascript or HTML) here.
-
-if ('this_is' == /an_example/) {
-    of_beautifier();
-} else {
-    var a = b ? (c % d) : e[f];
-}
+})); 
